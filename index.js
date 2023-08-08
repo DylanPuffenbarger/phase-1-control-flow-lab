881052ec5c225099287ec67821a37320ce437bb7
Dylan Puffenbarger <dylanpuffenbarger@gmail.com>
2023-08-08 12:34:57 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(dist){
   // Write your code here!
+  if (dist <= 400) {
+    return 'This one is on me!';
+  } else if (dist <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (dist <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  let isNYC;
+  cityName == 'NYC'?(isNYC = true):(isNYC = false);
+  if (isNYC){
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  switch(tip){
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.';
+      break;
+  }
+  return message;
 }
